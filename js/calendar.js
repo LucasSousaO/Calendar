7eba2b21fb2b78cadc4405387d7cd2d2b4afabd1
Lucas Sousa <lucas02s.oliveira@gmail.com>
2019-04-29 15:58:38 -0300

    utilizando vue.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,45 @@
 let dateToday = new Date();
+
+let dateMonth = dateToday.getMonth()+1;
+
+let dateDay = dateToday.getDay(); // Sunday - Saturday : 0 - 6
+let dateDayNumber = dateToday.getDate();
+let dateYear = dateToday.getFullYear();
+let lastDay = new Date(2019, dateMonth, 0).getDate();
+
+const yearsMonth = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let monthName = yearsMonth[dateMonth-1];
+let testeDate = "DD/MM/YYYY";
+const $weekDays = $(".week-days");
+let day = "";
+
+
+new Vue({
+    el: "#calendar",
+    data:{
+        date: dateDayNumber+"/"+dateMonth+"/"+dateYear,
+        day: dateDayNumber, 
+        month: monthName,
+        year: dateYear,
+        weekDay: dateDay,
+        months: [{mes:"January"},{mes: "February"},{mes: "March"},{mes:"April"},{mes: "May"},{mes: "June"},{mes: "July"},{mes: "August"},{mes: "September"},{mes: "October"},{mes: "November"},{mes: "December"}],
+        weekDay: [{dia:"Sunday"}, {dia:"Monday"}, {dia:"Tuesday"}, {dia:"Wednesday"}, {dia:"Thursday"}, {dia:"Friday"}, {dia:"Saturday"}]
+    },
+    computed:{
+        
+    }
+})
+
+for(i=0; i < weekDay.length; i++){                                          
+    day = ("<ul class="+weekDay[i]+">"+ weekDay[i] + "</ul><br>");          
+    $weekDays.append(day);
+};
+
+
+/*
+
+let dateToday = new Date();
 let dateMonth = dateToday.getMonth()+1;
 let dateDay = dateToday.getDay(); // Sunday - Saturday : 0 - 6
 let dateDayNumber = dateToday.getDate();
@@ -34,3 +75,4 @@ let doSomething = (diasVazios) =>{
                 $("."+ weekDay[testeDate.getDay()]).append("<br><ul>"+dia+"</ul>");
         };
 
+*/
