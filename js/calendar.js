80da80ec84a3773739ffafd574e168d263c8dcb5
LucasSousa <lucas02s.oliveira@gmail.com>
2019-05-01 11:08:46 -0300

    Incluindo function to start

--- js/calendar.js
+++ js/calendar.js
@@ -3,13 +3,12 @@ let dateToday = new Date();
 const yearsMonth = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-
 new Vue({
     el: "#calendar",
         data:{
             date: dateToday.getDate()+"/"+dateToday.getMonth()+"/"+dateToday.getFullYear(), //DD/MM/YYYY
             day: dateToday.getDate(),                                                         //DD
-            month: yearsMonth[dateToday.getMonth()+1-1],                                      //MMMM
+            month: yearsMonth[dateToday.getMonth()],                                      //MMMM
             year: dateToday.getFullYear(),                                                    //YYYY
            
             lastDay: new Date(2019, dateToday.getMonth()+1, 0).getDate(),
@@ -22,7 +21,9 @@ new Vue({
         methods:{
             firstUpdate: function() {
                let actualDay = dateToday.getDate();
-               let actualMonth = dateToday.getMonth();
+               /*let actualMonth = dateToday.getMonth();
+
+               
                 for(dd=0;dd<7;dd++){ //create table title
                     $(".table-month").append("<ul id="+weekDay[dd]+">"+weekDay[dd]+"</ul>");
                 }
@@ -45,7 +46,7 @@ new Vue({
                             (dd == actualDay)&&(actualMonth==newDate.getMonth())? insertDay = "<ul class= today " + weekDay[newDate.getDay()] +" id="+dd+">"+dd+"</ul>": insertDay = "<ul class= " + weekDay[newDate.getDay()] +" id="+dd+">"+dd+"</ul>";
                                 //let insertDay = "<ul class= " + weekDay[newDate.getDay()] +">"+dd+"</ul>";
                                 $("#"+ weekDay[newDate.getDay()]).append(insertDay)
-                        }
+                        }*/
 
                
             },
@@ -56,11 +57,11 @@ new Vue({
                     let newDate= new Date(this.month + ","+ 01  +",2019"); //dateToday1 = new Date("03,03,2019");
                     let lastDay = new Date(2019, newDate.getMonth()+1, 0).getDate();
                     let firstDay = weekDay[newDate.getDay()];
-
+                        //CLEANING THE FULL TABLE
                     for(let dd = 1; dd <= lastDay; dd++){
                         $("#"+dd).remove();
                         }
-                    for(dd=0;dd<7;dd++){ //create table title
+                    for(dd=0;dd<7;dd++){ 
                          $("#"+weekDay[dd]).remove();
                          }
 
@@ -94,6 +95,7 @@ new Vue({
                 reversa: function(){
                     this.month = this.month.split('').reverse().join('');
                     $("#botao").text("Reverse");
+                    
                 },
         },
 });
@@ -103,3 +105,37 @@ Vue.component('blog-post', {
     props: ['tableDay'],
     template: '<ul>{{ tableDay }}</ul>'
   });*/
+
+  let comeca = function(month){
+    let actualDay = new Date().getDate();
+    let actualMonth = new Date().getMonth();
+    
+     for(dd=0;dd<7;dd++){ //create table title
+         $(".table-month").append("<ul id="+weekDay[dd]+">"+weekDay[dd]+"</ul>");
+     }
+     //create table title
+     let lastDay = new Date(2019, new Date(month + ","+ 01  +",2019").getMonth(), 0).getDate();
+     let firstDay = weekDay[new Date((month+1) + ","+ 01  +",2019").getDay()]; 
+
+
+     let ajustFirstDay =0;
+     firstDay != "Monday"?  ajustFirstDay = 0 : ajustFirstDay =new Date((month+1) + ","+ 01  +",2019").getDay();
+         //empty slot
+         
+         while(ajustFirstDay<new Date(month + ","+ 01  +",2019").getDay()){
+                     let insertDay = "<ul>&nbsp</ul>";
+                     $("#"+ weekDay[ajustFirstDay]).append(insertDay);
+             ajustFirstDay++;
+             console.log("campo em branco");
+         }  
+             //create the real table
+             for(let dd = 1; dd <= lastDay; dd++){
+                 let insertDay ='';
+                 newDate = new Date(month + ","+ dd  +",2019");
+                 (dd == actualDay)&&(actualMonth==newDate.getMonth())? insertDay = "<ul class= today " + weekDay[newDate.getDay()] +" id="+dd+">"+dd+"</ul>": insertDay = "<ul class= " + weekDay[newDate.getDay()] +" id="+dd+">"+dd+"</ul>";
+                     //let insertDay = "<ul class= " + weekDay[newDate.getDay()] +">"+dd+"</ul>";
+                     $("#"+ weekDay[newDate.getDay()]).append(insertDay)
+             }
+  }
+
+  comeca(new Date().getMonth());
