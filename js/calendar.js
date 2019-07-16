01639e141d2667d128a97d2d08b19dcbbd0f477f
Lucas Sousa <40129564+LucasSousaO@users.noreply.github.com>
2019-07-16 15:32:59 -0300

    testing my commit

--- js/calendar.js
+++ js/calendar.js
@@ -1,3 +1,30 @@
+let locale = 'Belo Horizonte';
+let state = "MG";
+let initial = 'http://apiadvisor.climatempo.com.br';
+
+//function - axios.get(initialLink+secondLink+'?limit=50&apikey='+key).then(function(response)
+
+// api key weather prediction b08f2d138c8c92fba15efc2031a4e0df
+const key = 'b08f2d138c8c92fba15efc2031a4e0df';
+let temperatura = 0;
+//FIND ID
+let id = '';
+// search for id /api/v1/locale/city?name=CityName&state=StateAbbr&token=your-app-token
+let idLocale = axios.get(initial+'/api/v1/locale/city?name='+locale+'&state='+state+'&token='+key).then(function(response){
+
+    let id= response.data[0].id;
+//FIND THE WEATHER
+const clima = '/api/v1/weather/locale/'
+// weather - real time get/  /api/v1/weather/locale/:id/current?token=your-app-token
+//ex. curl -i http://apiadvisor.climatempo.com.br/api/v1/weather/locale/3477/current?token=your-app-token
+let weather = axios.get(initial+clima+id+'/current?token='+key).then(function(response){
+
+let sensacao = (response.data.data.sensation);
+let temperatura = (response.data.data.temperature);
+let humidade = (response.data.data.humidity);
+//já com dados de temperatura, humdade e sensação termica
+
+
 let dateToday = new Date();
 
 const yearsMonth = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -18,20 +45,24 @@ new Vue({
             DDDD: weekDay[dateToday.getDay()].toUpperCase(),                                   //WEEK DAY UPPERCASE
             months: [{mes: "January", id: "0"},{mes: "February", id: "1"},{mes: "March", id: "2"},{mes:"April", id: "3"},{mes: "May", id: "4"},{mes: "June", id: "5"},{mes: "July", id: "6"},{mes: "August", id: "7"},{mes: "September", id: "8"},{mes: "October", id: "9"},{mes: "November", id: "10"},{mes: "December", id: "11"}],
             weekDia: weekDay[dateToday.getDate()],
-            show: true,                                              //WEEK DAY
+            show: true,
+            se: false,                                            //WEEK DAY
+            weather: temperatura,
+            humidity: humidade,
+            sensation: sensacao,
+            city: locale,
         },
-        computed:{
-            //ATUALIZA HORA - NÃO ESTÁ FUNCIONANDO
-           /* HH(){
-               return this.HH != new Date().getHours()?new Date().getHours(): this.HH;
+        watch:{
+            //ATUALIZA HORA - NÃO ESTÁ FUNCIONANDO automaticamente
+            hourUpdate: function(){
+                this.HH = new Date().getHours();    
             },
-            MM(){
-                return this.HH != new Date().getHours()?new Date().getHours(): this.HH;
+            minutesUpdate: function(){                                                //HH
+                this.MM = new Date().getMinutes();  
             },
-            SS: function (){
-                console.log('new: %s, old: %s', SS, new Date().getSeconds())
-                return 
-            },*/
+            secondsUpdate: function (){                                             //MM
+                this.SS = new Date().getSeconds(); 
+            },
   
         },
 
@@ -40,12 +71,12 @@ new Vue({
         methods:{
                 
             monthUpdate: function(newMonth){
-                this.show = false;
-                //this.month.addClass("escolhido");
-                //GET THE MONTH
-                setTimeout(()=>{
-                    this.show = true;
-                    console.log("voltou");
+
+                    //this.se == false ? (this.se = true) : (this.se = false) ;
+
+                    //this.show = false;
+                    //setTimeout(function(){this.show = true;
+                    //console.log("mudou também");},5000);
 
                     this.month = yearsMonth[newMonth];
                     //UPDATE FOR THE NEXT MONTH
@@ -81,9 +112,6 @@ new Vue({
                         (dd == actualDay)&&(actualMonth==newDate.getMonth())? insertDay = "<ul class= today " + weekDay[newDate.getDay()] +" id="+dd+">"+dd+"</ul>": insertDay = "<ul class= " + weekDay[newDate.getDay()] +" id="+dd+">"+dd+"</ul>";
                             $("#"+ weekDay[newDate.getDay()]).append(insertDay)
                     }
-                //this.show = true;  
-                }, 800);
-                
             },
             createWeek: function(){
                 console.log("criei semana");
@@ -91,12 +119,13 @@ new Vue({
                     $(".table-month").append("<ul id="+weekDay[dd]+" class='week-days'>"+weekDay[dd]+"</ul>");
                 }
             },
-            beforeEnter: function (el) {
-                el.style.opacity = 0
+/*            beforeEnter: function (el) {
+                el.style.opacity = 1
             },
             enter: function (el, done) {
-                Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
-                //monthUpdate(4);
+            //    console.log("tentativa");
+             //   this.createWeek;
+                //Velocity(el, { opacity: 1, fontSize: '1em', display: "flex" }, { duration: 300 })
                 Velocity(el, { fontSize: '1em' }, { complete: done })
             },
             leave: function (el, done) {
@@ -105,8 +134,17 @@ new Vue({
                     translateY: '30px',
                     translateX: '30px',
                     opacity: 0
-                }, { complete: done })
-            }
+                })
+                Velocity(el, { translateX: '0px',translateY: '00px', rotateZ: '0deg', transformOrigin: ["X Y Z","00% 00% 00px"] }, { complete: done })
+            },
+*/
+            updateTime: function(){
+                console.log("The time was updated!");
+                this.HH = new Date().getHours();                                                         //HH
+                this.MM = new Date().getMinutes();                                                         //MM
+                this.SS = new Date().getSeconds();
+            },
+            
         },
         mounted: () => {
                         let actualDay = dateToday.getDate();
@@ -124,7 +162,7 @@ new Vue({
         
                         let ajustFirstDay =0;
         
-                        firstDay != "Monday"?  ajustFirstDay = 0 : ajustFirstDay = ajuste;
+                        firstDay != "Sunday"?  ajustFirstDay = 0 : ajustFirstDay = ajuste;
                         //empty slot
 
                         while(ajustFirstDay<ajuste){
@@ -146,8 +184,6 @@ new Vue({
 
 });
 
-
-  
-  
-
-    
+//fim de dados.
+});
+});
